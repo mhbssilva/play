28e8c632388c3b5274807cf1851c8dc7e7b0e40b
Marcos H B S Silva <mhbssilva@gmail.com>
2020-05-18 09:07:42 -0300

    Creates base data structure

--- src/components/featured-content/featured-content.tsx
+++ src/components/featured-content/featured-content.tsx
@@ -7,15 +7,17 @@ import PlayIcon from "../icon/play";
 function FeaturedContent() {
   return (
     <section className="featured-content-wrapper">
-      <h1>{featuredContentData.title}</h1>
-      <div className="actions">
-        <Button
-          icon={<PlayIcon />}
-          label="Assista"
-          onClick={() => {}}
-          onFocus={() => {}}
-        />
-        <Button label="Veja mais" onClick={() => {}} onFocus={() => {}} />
+      <div className="featured-content">
+        <h1>{featuredContentData.title}</h1>
+        <div className="actions">
+          <Button
+            icon={<PlayIcon />}
+            label="Assista"
+            onClick={() => {}}
+            onFocus={() => {}}
+          />
+          <Button label="Veja mais" onClick={() => {}} onFocus={() => {}} />
+        </div>
       </div>
     </section>
   );
---
+++ src/data/data.tsx
@@ -0,0 +1,67 @@
+import IContentDataList from "../shared/interfaces/content-data-list";
+import bbbLogo from "../assets/images/bbb-logo.png";
+import bbbFeaturedContentBg from "../assets/images/bbb-featured-content-bg.jpg";
+import bbbTrailBg from "../assets/images/bbb-trail-bg.jpg";
+import bbbThumb1 from "../assets/images/bbb-image-1.jpg";
+import bbbThumb2 from "../assets/images/bbb-image-2.jpg";
+import bbbThumb3 from "../assets/images/bbb-image-3.jpg";
+import bbbThumb4 from "../assets/images/bbb-image-4.jpg";
+import bbbThumb5 from "../assets/images/bbb-image-5.jpg";
+import bbbThumb6 from "../assets/images/bbb-image-6.jpg";
+
+const sampleData: IContentDataList = {
+  contents: [
+    {
+      type: "feature",
+      programLogoImage: bbbLogo,
+      data: {
+        title: "Acompanhe 24h ao vivo a casa mais vigiada do Brasil",
+        backgroundImage: bbbFeaturedContentBg,
+        actions: {
+          play: true,
+          more: true,
+        },
+      },
+    },
+    {
+      type: "trail",
+      trailName: "Agora no BBB",
+      categoryName: "Realities",
+      programName: "Big Brother Brasil",
+      data: [
+        {
+          title: "Thelma é a campeã do BBB20, com 44,1% dos votos",
+          backgroundImage: bbbTrailBg,
+          thumbImage: bbbThumb5,
+        },
+        {
+          title: "Tiago se emociona na Final do BBB20",
+          backgroundImage: bbbTrailBg,
+          thumbImage: bbbThumb1,
+        },
+        {
+          title: "Programa de 27/04/2020",
+          backgroundImage: bbbTrailBg,
+          thumbImage: bbbThumb2,
+        },
+        {
+          title: "Bate-Papo BBB com Babu, último eliminado",
+          backgroundImage: bbbTrailBg,
+          thumbImage: bbbThumb3,
+        },
+        {
+          title: "Prêmio Rede BBB homenageia brothers",
+          backgroundImage: bbbTrailBg,
+          thumbImage: bbbThumb4,
+        },
+        {
+          title: "Tiago Leifert pergunta para sisters o que é o BBB",
+          backgroundImage: bbbTrailBg,
+          thumbImage: bbbThumb6,
+        },
+      ],
+    },
+  ],
+};
+
+export default sampleData;
